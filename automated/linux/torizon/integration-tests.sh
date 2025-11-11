d1901bce36c1ce04282d369ed6c0e3455112b0f4
Pawel Szymaszek <pawel.szymaszek@linaro.org>
2025-11-11 10:30:09 +0100

    test run of new script version

--- automated/linux/torizon/integration-tests.sh
+++ automated/linux/torizon/integration-tests.sh
@@ -45,9 +45,8 @@ git checkout "$BRANCH_NAME"
 export SPIRE_PAT_TOKEN LAVA_TOKEN LAVA_PASSWORD SQUAD_UPLOAD_URL SQUAD_ARCHIVE_SUBMIT_TOKEN
 
 # run tests
-#uv run robot --pythonpath . --exclude gitlab_pipeline --variable remote:"$IS_REMOTE" --outputdir=.. --listener test/keyword_listener.py test/
+uv run robot --pythonpath . --exclude gitlab_pipeline --variable remote:"$IS_REMOTE" --outputdir=.. --listener test/keyword_listener.py test/
 
-#cd ..
-#../../../utils/upload-to-squad.sh -a ../output.xml -u "$SQUAD_UPLOAD_URL"
+../../../utils/upload-to-squad.sh -a ../output.xml -u "$SQUAD_UPLOAD_URL"
 uv run --project ../../../utils/ uv run ../../../utils/parse-robot-framework.py -r ../output.xml
 exit 0
