c2e381315fa55acf6c74ef1ff8e54b942cefd2bc
Pawel Szymaszek <pawel.szymaszek@linaro.org>
2025-11-17 14:47:55 +0100

    newest script version

--- automated/linux/torizon/integration-tests.sh
+++ automated/linux/torizon/integration-tests.sh
@@ -4,6 +4,8 @@
 set -x
 . ../../lib/sh-test-lib
 
+UTILS_PATH="../../../utils"
+
 # source the secrets file to get the gitlab_token env var
 lava_test_dir="$(
   dir="$PWD"
@@ -14,9 +16,9 @@ lava_test_dir="$(
   sort -t- -k2,2n |
   tail -1
 )"
-    if test -f "${lava_test_dir}/secrets"; then
-        . "${lava_test_dir}/secrets"
-    fi
+if test -f "${lava_test_dir}/secrets"; then
+. "${lava_test_dir}/secrets"
+fi
 
 # Determine the appropriate packages for the architecture
 SPIRE_VERSION="0.3.4"
@@ -24,16 +26,17 @@ GECKO_VERSION="v0.36.0"
 UV_VERSION="0.9.4"
 
 detect_abi
+# shellcheck disable=SC2154
 case "${abi}" in 
-	x86_64)
+  x86_64)
     GECKODRIVER_ARCH="linux64"
     SPIRE_ARCH="linux_amd64"
     ;;
-arm64|aarch64)
+  arm64|aarch64)
     GECKODRIVER="linux-aarch64"
     SPIRE_ARCH="linux_arm64"
     ;;
-*)
+  *)
     echo "Unknown architecture: ${abi}"
     exit 1
     ;;
@@ -57,15 +60,12 @@ curl -LsSf "https://astral.sh/uv/${UV_VERSION}/install.sh" | sh
 . "$HOME"/.local/bin/env
 
 # clone baklava-integration repo and install required pip pkgs
-get_test_program "https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/LinaroLtd/lava/appliance/baklava-integration/docker-tests.git" "docker-tests" "main"
-git checkout "$BRANCH_NAME"
+get_test_program "https://gitlab-ci-token:${GITLAB_TOKEN}@gitlab.com/LinaroLtd/lava/appliance/baklava-integration/docker-tests.git" "docker-tests" "$BRANCH_NAME"
 
 export SPIRE_PAT_TOKEN LAVA_TOKEN LAVA_PASSWORD SQUAD_UPLOAD_URL SQUAD_ARCHIVE_SUBMIT_TOKEN
 
 # run tests with uv
-# uv run robot --pythonpath . --exclude gitlab_pipeline --variable remote:"$IS_REMOTE" --outputdir=.. --listener test/keyword_listener.py test/
-
-UTILS_PATH="../../../utils"
+uv run robot --pythonpath . --exclude gitlab_pipeline --variable remote:"$IS_REMOTE" --outputdir=.. --listener test/keyword_listener.py test/
 
 "${UTILS_PATH}"/upload-to-squad.sh -a ../output.xml -u "$SQUAD_UPLOAD_URL"
 uv run --project "${UTILS_PATH}"/ uv run "${UTILS_PATH}"/parse-robot-framework.py -r ../output.xml
