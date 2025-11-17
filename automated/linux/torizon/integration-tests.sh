d4066a04ec5d21a21da4fa2f94156c4b10e79850
Pawel Szymaszek <pawel.szymaszek@linaro.org>
2025-11-17 08:19:17 +0100

    all changes required by AR

--- automated/linux/torizon/integration-tests.sh
+++ automated/linux/torizon/integration-tests.sh
@@ -5,37 +5,48 @@ set -x
 . ../../lib/sh-test-lib
 
 # source the secrets file to get the gitlab_token env var
-. ../../../../../../secrets > /dev/null 2>&1
 
-# temp hack for aarch64 geckodriver
-ARCH=$(uname -m)
+lava_test_dir="$(find /lava-* -maxdepth 0 -type d | grep -E '^/lava-[0-9]+' 2>/dev/null | sort | tail -1)"
+    if test -f "${lava_test_dir}/secrets"; then
+        . "${lava_test_dir}/secrets"
+    fi
+
+# Determine the appropriate packages for the architecture
 SPIRE_VERSION="0.3.4"
+GECKO_VERSION="v0.36.0"
+UV_VERSION="0.9.4"
 
-if [ "$ARCH" = "x86_64" ]; then
-    DRIVER="geckodriver-v0.36.0-linux64.tar.gz"
-    SPIRE="staging-spire_${SPIRE_VERSION}_linux_amd64.deb"
-
-elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
-    DRIVER="geckodriver-v0.36.0-linux-aarch64.tar.gz"
-    SPIRE="staging-spire_${SPIRE_VERSION}_linux_arm64.deb"
-else
+detect_abi
+case "${abi}" in 
+	x86_64)
+    GECKODRIVER_ARCH="linux64"
+    SPIRE_ARCH="linux_amd64"
+    ;;
+arm64|aarch64)
+    DRIVER="linux-aarch64"
+    SPIRE="linux_arm64"
+    ;;
+*)
     echo "Unknown architecture: $ARCH"
     exit 1
-fi
+    ;;
+esac
+
+GECKODRIVER="geckodriver-${GECKO_VERSION}-${GECKODRIVER_ARCH}.tar.gz"
+SPIRE="staging-spire_${SPIRE_VERSION}_${SPIRE_ARCH}.deb"
 
 # Download and install spire package
 curl -sSLO "https://github.com/Linaro/SPIRE-CLI-S-/releases/download/$SPIRE_VERSION/$SPIRE"
 dpkg -i "$SPIRE"
 
 # Download and install gecko driver
-curl -LO "https://github.com/mozilla/geckodriver/releases/download/v0.36.0/$DRIVER"
+curl -LO "https://github.com/mozilla/geckodriver/releases/download/${GECKO_VERSION}/$GECKODRIVER"
 tar -xf "$DRIVER"
 mv geckodriver /usr/local/bin
 chown root:root /usr/local/bin/geckodriver
 
 # Download and install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
+curl -LsSf "https://astral.sh/uv/${UV_VERSION}/install.sh" | sh
 . "$HOME"/.local/bin/env
 
 # clone baklava-integration repo and install required pip pkgs
@@ -44,9 +55,10 @@ git checkout "$BRANCH_NAME"
 
 export SPIRE_PAT_TOKEN LAVA_TOKEN LAVA_PASSWORD SQUAD_UPLOAD_URL SQUAD_ARCHIVE_SUBMIT_TOKEN
 
-# run tests
+# run tests with uv
 uv run robot --pythonpath . --exclude gitlab_pipeline --variable remote:"$IS_REMOTE" --outputdir=.. --listener test/keyword_listener.py test/
 
 ../../../utils/upload-to-squad.sh -a ../output.xml -u "$SQUAD_UPLOAD_URL"
 uv run --project ../../../utils/ uv run ../../../utils/parse-robot-framework.py -r ../output.xml
+
 exit 0
